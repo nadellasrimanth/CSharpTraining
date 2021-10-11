7fbf3f4ac694bb2bded6b1523b5a31cbe94a21a8
SrimanthNadella <90451233+SrimanthNadella@users.noreply.github.com>
2021-10-11 22:01:32 +0530

    Praticeing changes for c sharp with selenium

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Bogus;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CSharpPractice
+{
+    public class genname
+    {
+        public static void Main(string[] args)
+        {
+            genname dd = new genname();
+            dd.Start();
+        }
+
+
+        //selenium script
+        public ChromeDriver driver;
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.demoqa.com/";
+            Thread.Sleep(9000);
+            driver.ExecuteScript("scroll(0,100)");
+            driver.FindElement(By.XPath("//h5[text()='Elements']")).Click();
+            WebTables();
+        }
+        public void WebTables()
+        {
+            RandomUserDetails rum = new RandomUserDetails();
+            var userdd = rum.GenerateRandomPerson();
+            //checking with passing values submit
+            driver.FindElement(By.XPath("//span[contains(@class,'text')]  [contains(text(),'Web Tables')]")).Click();
+            driver.FindElement(By.XPath("//button[contains(@id,'addNewRecordButton')]  [contains(text(),'Add')]")).Click();
+            driver.FindElement(By.XPath("//input[contains(@id,'firstName')]")).SendKeys(userdd.FirstName);
+            driver.FindElement(By.XPath("//input[contains(@id,'lastName')]")).SendKeys(userdd.LastName);
+            driver.FindElement(By.XPath("//input[contains(@id,'userEmail')]")).SendKeys(userdd.Email);
+            driver.FindElement(By.XPath("//input[contains(@id,'age')]")).SendKeys(userdd.Age.ToString());
+            driver.FindElement(By.XPath("//input[contains(@id,'salary')]")).SendKeys(userdd.Salary.ToString());
+            driver.FindElement(By.XPath("//input[contains(@id,'department')]")).SendKeys(userdd.Department);
+            driver.FindElement(By.XPath("//button[contains(@id,'submit')]")).Click();
+
+            
+        }
+
+    }
+}
---
+++ RandomUserDetails.cs
@@ -0,0 +1,30 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class RandomUserDetails
+    {
+        public UserDetails GenerateRandomPerson()
+        {
+            Random random = new Random();
+            var age = random.Next(25, 99);
+            var sal = random.Next(10000, 999999);
+            var fakeData = new Faker<UserDetails>()
+                .RuleFor(c => c.FirstName, f => f.Name.FirstName())
+                .RuleFor(c => c.LastName, f => f.Name.LastName())
+                .RuleFor(c => c.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
+                .RuleFor(c => c.CurrentAddress, f => f.Address.FullAddress())
+                .RuleFor(c => c.PermanentAddress, f => f.Address.FullAddress())
+                .RuleFor(c => c.Department, f => f.Company.CompanySuffix())
+                .RuleFor(c => c.Salary, sal)
+                .RuleFor(c => c.Age, age);
+            var data = fakeData.Generate();
+            return data;
+        }
+    }
+}
